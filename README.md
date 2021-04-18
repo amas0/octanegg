7c89b94f579e3d98330205707f4f63be44176f76
amas <andrew.mascioli1@gmail.com>
2021-04-17 21:20:53 -0400

    Change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# octane-py 
+# octanegg
 
 This library is a lightweight API client for [octane.gg](https://octane.gg), a site that provides
 statistics on professional Rocket League.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='octane-py',
+setup(name='octanegg',
       version='0.1',
       author='amas0',
       description='Python client library for octane.gg public API',
