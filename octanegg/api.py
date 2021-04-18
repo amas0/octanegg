410e5fe48f0777eaf71cafe2d56a57be35fcbfa3
amas <andrew.mascioli1@gmail.com>
2021-04-17 21:21:09 -0400

    Add get events and refactor

--- octanegg/api.py
+++ octanegg/api.py
@@ -28,27 +28,41 @@ class Octane:
     def _date_to_str(d: date) -> str:
         return d.strftime('%Y-%m-%d')
 
+    def _handle_date_params(self, before: Union[str, date, None], after: Union[str, date]) -> dict:
+        out = {'after': self._date_to_str(after) if isinstance(after, date) else after}
+        if before is not None:
+            out['before'] = self._date_to_str(before) if isinstance(before, date) else before
+        else:
+            out['before'] = self._date_to_str(date.today())
+        return out
+
     def get_events(self, after: Union[str, date], before: Optional[Union[str, date]] = None,
                    name: Optional[str] = None, tier: Optional[str] = None, region: Optional[str] = None,
                    mode: Optional[int] = None, sort: Optional[str] = None, order: Optional[str] = None,
-                   page: Optional[int] = None, per_page: Optional[str] = None):
+                   page: Optional[int] = None, per_page: Optional[str] = '20') -> list:
         endpoint = f'{API_BASE_URL}/events'
-        params = {
-            'after': self._date_to_str(after) if isinstance(after, date) else after,
-            'per_page': '20' if per_page is None else per_page  # This option seems broken, setting it anyways
-        }
-        if before is not None:
-            params['before'] = self._date_to_str(before) if isinstance(before, date) else before
-        else:
-            params['before'] = self._date_to_str(date.today())
-
         other_params = {'name', 'tier', 'region', 'mode', 'sort', 'order', 'page'}
+
+        params = self._handle_date_params(before, after)
         params |= {k: v for k, v in locals().items() if (k in other_params) and (v is not None)}
 
         results = self._get_results(endpoint, params).get('events')
         return results
 
-    def get_event(self, event_id: str):
+    def get_event(self, event_id: str) -> dict:
         endpoint = f'{API_BASE_URL}/events/{event_id}'
         result = self._get_results(endpoint, {})
         return result
+
+    def get_matches(self, after: Union[str, date], before: Optional[Union[str, date]] = None,
+                    event: Optional[str] = None, stage: Optional[int] = None, substage: Optional[int] = None,
+                    sort: Optional[str] = None, order: Optional[str] = None, page: Optional[int] = None,
+                    per_page: Optional[str] = '20') -> list:
+        endpoint = f'{API_BASE_URL}/matches'
+        other_params = {'event', 'stage', 'substage', 'sort', 'order', 'page'}
+
+        params = self._handle_date_params(before, after)
+        params |= {k: v for k, v in locals().items() if (k in other_params) and (v is not None)}
+
+        results = self._get_results(endpoint, params).get('matches')
+        return results
