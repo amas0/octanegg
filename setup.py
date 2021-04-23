beebb3216f8a67327659d87ad7f1f3d525e247f4
amas <andrew.mascioli1@gmail.com>
2021-04-22 22:48:23 -0400

    Forgot packages arg in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='octanegg',
       version='0.1',
       author='amas0',
       description='Python client library for octane.gg public API',
+      packages=find_packages(),
       install_requires=[
             'requests'
       ])
