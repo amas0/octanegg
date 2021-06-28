0d19ed802f86c138e5face9570ef27345e6f94d4
amas <andrew.mascioli1@gmail.com>
2021-06-28 19:23:44 -0400

    Update setup for pypi packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='octanegg',
-      version='0.2',
+      version='1.0',
       author='amas0',
+      author_email='andrew.mascioli1@gmail.com',
       description='Python client library for octane.gg public API',
+      url='https://github.com/amas0/octanegg',
       packages=find_packages(),
       install_requires=[
             'requests'
-      ])
+      ],
+      python_requires='>=3.7')
